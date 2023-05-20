e62757b01cc51504de4ab618e882d5975e8a41b4
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-05-21 01:58:24 +0700

    fix warning build

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     base: '/vku-score',
+    build: {
+        chunkSizeWarningLimit: 10000,
+    },
 });
